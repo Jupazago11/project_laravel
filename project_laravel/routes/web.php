f93c16c16662c1f7864c17673db41e038c49617d
Jupazago11 <jupazago11@gmail.com>
2025-04-02 19:18:25 -0500

    Actualización del README con los pasos realizados

---
+++ project_laravel/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hola', function () {
+    return '¡Hola Mundo desde Laravel!';
+});
